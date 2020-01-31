866a55437abbbf144f4fdf81ddfbe7f753ed699d
Allie Robinson <allierob93@gmail.com>
2020-01-31 13:51:12 -0600

    hit mvp

--- users/tests.spec.js
+++ users/tests.spec.js
@@ -59,44 +59,82 @@ const request = supertest(server)
 
 
 // This test works! It will work every time, no need to change anything. 
-describe('test login', function () {
-    it('shows status 200', async function (done) {
-        request
-            .post('/api/auth/login')
-            .send({ username: 'chance', password: '111111' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-                done();
-              });
+// describe('test login', function () {
+//     it('shows status 200', async function (done) {
+//         request
+//             .post('/api/auth/login')
+//             .send({ username: 'chance', password: '111111' })
+//             .set('Accept', 'application/json')
+//             .expect('Content-Type', /json/)
+//             .expect(200)
+//             .end(function(err, res) {
+//                 if (err) return done(err);
+//                 done();
+//               });
+//     });
+
+//     it('shows status 401', async function (done) {
+//         request
+//             .post('/api/auth/login')
+//             .send({ username: 'chance', password: '211111' })
+//             .set('Accept', 'application/json')
+//             .expect('Content-Type', /json/)
+//             .expect(401)
+//             .end(function(err, res) {
+//                 if (err) return done(err);
+//                 done();
+//               });
+//     })  
+
+//     it('shows status 500', async function (done) {
+//         request
+//             .post('/api/auth/login')
+//             .send({ name: 'chance', password: '111111' })
+//             .set('Accept', 'application/json')
+//             .expect('Content-Type', /json/)
+//             .expect(500)
+//             .end(function(err, res) {
+//                 if (err) return done(err);
+//                 done();
+//               });
+//     })  
+// });   
+
+
+let token;
+
+    beforeAll((done) => {
+      request
+        .post('/api/auth/login')
+        .send({
+          username: 'chance',
+          password: '111111',
+        })
+        .end((err, response) => {
+          token = response.body.token; // save the token!
+          console.log(token)
+          done();
+        });
+    });          
+
+    describe('GET /', () => {
+      // token not being sent - should respond with a 401
+      test('It should require authorization', () => {
+        return request
+          .get('/api/jokes')
+          .then((response) => {
+            expect(response.statusCode).toBe(401);
+          });
+      });
+      // send the token - should respond with a 200
+      test('It responds with JSON', () => {
+        return request
+          .get('/api/jokes')
+          .set('Authorization', `${token}`)
+          .then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.type).toBe('application/json');
+          });
+      });  
     });
-
-    it('shows status 401', async function (done) {
-        request
-            .post('/api/auth/login')
-            .send({ username: 'chance', password: '211111' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(401)
-            .end(function(err, res) {
-                if (err) return done(err);
-                done();
-              });
-    })  
-
-    it('shows status 500', async function (done) {
-        request
-            .post('/api/auth/login')
-            .send({ name: 'chance', password: '111111' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(500)
-            .end(function(err, res) {
-                if (err) return done(err);
-                done();
-              });
-    })  
-});    
 
